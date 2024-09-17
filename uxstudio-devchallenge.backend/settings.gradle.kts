4e109593f8fdb9ddf1860c250cc23b51ef9c5e9d
Bella Barnabás <barnabasbella@gmail.com>
2024-09-17 23:06:19 +0200

    Initial commit

---
+++ uxstudio-devchallenge.backend/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "uxstudio-devchallenge.backend"
---
+++ uxstudio-devchallenge.backend/src/main/kotlin/com/example/uxstudio_devchallenge/backend/Application.kt
@@ -0,0 +1,11 @@
+package com.example.uxstudio_devchallenge.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+	runApplication<Application>(*args)
+}
