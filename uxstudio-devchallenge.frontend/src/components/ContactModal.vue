cab270b659d27ce0754764718423d0109793bd93
Bella Barnabás <barnabasbella@gmail.com>
2024-09-24 21:41:35 +0200

    modal fixes

--- uxstudio-devchallenge.frontend/src/components/ContactModal.vue
+++ uxstudio-devchallenge.frontend/src/components/ContactModal.vue
@@ -84,10 +84,10 @@ const deletePicture = () => {
 <template>
     <div class="modal" v-if="isOpen">
         <div class="modal-content">
-            <h2 class="modal-content-header">
-                <slot name="header"></slot>
-            </h2>
             <div class="modal-content-body">
+                <h2 class="modal-content-header">
+                    <slot name="header"></slot>
+                </h2>
                 <div class="picture-container">
                     <img v-if="isContactPicture" :src="contactPicture" class="contact-pic" />
                     <DefaultIcon v-else class="contact-pic" />
@@ -101,7 +101,7 @@ const deletePicture = () => {
                                 Change picture
                             </template>
                         </FancyButton>
-                        <FancyButton :isPrimary="true" class="change-pic-button" v-else @click="uploadPicture">
+                        <FancyButton :isPrimary="true" class="add-pic-button" v-else @click="uploadPicture">
                             <template v-slot:icon>
                                 <AddIcon />
                             </template>
@@ -123,27 +123,28 @@ const deletePicture = () => {
                 </div>
                 <div class="modal-content-item">
                     <div class="modal-label">Phone number</div>
-                    <input type="text" name="input-phone" id="input-phone" v-model="contact.phoneNumber">
+                    <input type="text" name="input-phone" id="input-phone" v-model="contact.phoneNumber" maxlength="20">
                 </div>
                 <div class="modal-content-item">
                     <div class="modal-label">Email address</div>
                     <input type="text" name="input-email" id="input-email" v-model="contact.emailAddress">
                 </div>
+                <div class="modal-content-footer">
+                    <FancyButton :is-primary="false" @click="closeModal">
+                        <template v-slot:text>
+                            Cancel
+                        </template>
+                    </FancyButton>
+                    <FancyButton :is-primary="true" @click="saveModal">
+                        <template v-slot:text>
+                            Done
+                        </template>
+                    </FancyButton>
+                </div>
             </div>
-            <div class="modal-content-footer">
-                <FancyButton :is-primary="false" @click="closeModal">
-                    <template v-slot:text>
-                        Cancel
-                    </template>
-                </FancyButton>
-                <FancyButton :is-primary="true" @click="saveModal">
-                    <template v-slot:text>
-                        Done
-                    </template>
-                </FancyButton>
-            </div>
+
         </div>
-        <input type="file" ref="fileInput" @change="handleFileChange" class="hidden-upload" />
+        <input type="file" ref="fileInput" @change="handleFileChange" class="hidden-upload" accept="image/*" />
     </div>
 </template>
 
@@ -154,40 +155,57 @@ const deletePicture = () => {
     display: none;
 }
 
+.picture-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 316px;
+}
+
+.contact-pic {
+    width: 88px;
+    height: 88px;
+    border-radius: 44px;
+    border: 1px solid var(--grey-70);
+}
+
 .picture-button-container {
     display: flex;
     justify-content: space-between;
+    width: 212px;
 }
 
 .change-pic-button {
     width: 164px;
 }
 
-.delete-pic-button {
-    width: 40px;
+.add-pic-button {
+    width: 139px;
 }
 
-.modal-label {
-    margin-top: 24px;
+.delete-pic-button {
+    width: 40px;
 }
 
 .modal-content-body {
     display: flex;
     flex-direction: column;
-    justify-content: space-between;
+    justify-content: space-around;
+    height: 100%;
 }
 
 .modal-content-item {
     display: flex;
     flex-direction: column;
     justify-content: space-between;
+    height: 60px;
 }
 
 .modal-label {
     font-size: 12px;
     font-family: 'LexendDeca';
     color: var(--color-text-secondary);
-    margin-bottom: 4px;
+
 }
 
 .modal-content-item input {
@@ -196,6 +214,7 @@ const deletePicture = () => {
     border: 1px solid var(--grey-60);
     border-radius: 8px;
     padding: 12px;
+    height: 40px;
     color: var(--color-text-main);
     font-size: 14px;
     outline: 0;
@@ -223,12 +242,12 @@ const deletePicture = () => {
 .modal-content {
     min-width: 364px;
     height: 60%;
-    padding: 24px;
+    align-items: center;
     border-radius: 8px;
     background-color: var(--color-background);
     display: flex;
     flex-direction: column;
-    justify-content: space-evenly;
+    justify-content: space-around;
 }
 
 .modal-content-header {
@@ -238,24 +257,13 @@ const deletePicture = () => {
 .modal-content-footer {
     display: flex;
     justify-content: right;
+    padding: 24px 0px 24px 0px;
     width: 100%;
-    margin-top: 24px;
 }
 
-button {
+.modal-content-footer button {
     width: 68px;
     margin-left: 8px;
-}
 
-.contact-pic {
-    width: 88px;
-    height: 88px;
-    border-radius: 44px;
-    border: 1px solid var(--grey-70);
-}
-
-.picture-container {
-    display: flex;
-    align-items: center;
 }
 </style>
